847ab8e87779c3e817b73659e003bbcae7e8e95e
Dan McCracken <mccrackend@gmail.com>
2016-01-02 13:13:50 -0500

    completeing script for conf_web and proxy

--- droplet-manager.sh
+++ droplet-manager.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 OPTIONS="Update-Conference-Website Update-GDGGR-Website Update-Proxy Update-All Quit"
 
-CONFERENCE_WEB_REPO="mccrackend/conference_website"
-GDGGR_WEB_REPO="gar"
+CONF_WEB_REPO="mccrackend/conference_website"
+CONF_WEB_ALIAS="conf_web"
+CONF_WEB_PORT="3000"
+
 PROXY_REPO="gdggr/web-proxy"
+PROXY_ALIAS="proxy"
+PROXY_PORT="80"
+
+GDGGR_WEB_REPO="gar"
+
+function update-container {
+  clear
+  echo "=====> Updating docker image   =>" $1
+  echo "=====> Using a container alias =>" $2
+  echo "=====> And running it on port  =>" $3
+  echo
+
+  echo "=====>" Tagging current running container image as backup
+  docker tag -f $1:latest $1:backup
+  docker images
+
+  echo
+  echo "=====>" Pulling latest image from docker hub
+  docker pull $1
+
+  echo
+  echo "=====>" Killing current running container
+  docker ps -a
+  docker kill $2
+
+  echo
+  echo "=====>" Removing killed container
+  docker ps -a
+  docker rm $2
+
+  echo
+  echo "=====>" Starting up the latest container
+  docker run --name $2 -p $3:80 -d $1:latest
+  docker ps -a
+}
 
 function update-conference-website {
-  echo "Updating docker image for" $CONFERENCE_WEB_REPO
+  update-container $CONF_WEB_REPO $CONF_WEB_ALIAS $CONF_WEB_PORT
 }
 
 function update-gdggr-website {
@@ -14,7 +51,7 @@ function update-gdggr-website {
 }
 
 function update-proxy {
-  echo "Updating docker image for" $PROXY_REPO
+  update-container $PROXY_REPO $PROXY_ALIAS $PROXY_PORT
 }
 
 select opt in $OPTIONS; do
