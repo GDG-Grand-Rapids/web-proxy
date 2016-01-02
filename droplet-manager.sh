6e95a65ba09208cc2dbb8aa92b6a7b8bb28aac3d
Dan McCracken <mccrackend@gmail.com>
2016-01-02 16:40:03 -0500

    script tweak

--- droplet-manager.sh
+++ droplet-manager.sh
@@ -9,7 +9,9 @@ PROXY_REPO="gdggr/web-proxy"
 PROXY_ALIAS="proxy"
 PROXY_PORT="80"
 
-GDGGR_WEB_REPO="gar"
+GDGGR_WEB_REPO="mccrackend/conference_website"
+GDGGR_WEB_ALIAS="gdggr_web"
+GDGGR_WEB_PORT="172.17.42.1:5000"
 
 function update-container {
   clear
